e420b93bcdc268c987aebfe863699c788d6cace0
smk32 <1235477+smk32@users.noreply.github.com>
2025-04-11 09:40:17 -0400

    Quick updates

--- launch/gps.launch.py
+++ launch/gps.launch.py
@@ -50,11 +50,27 @@ def generate_launch_description():
                     get_params("oem7_supported_imus.yaml" ),
                     get_params("std_init_commands.yaml"   ),
                     {
+                    # Refer to README for more information on the configuration parameters
+                    # Receiver IO Parameters
                     'oem7_max_io_errors' : 10,
                     'oem7_msg_decoder': 'Oem7MessageDecoder',
                     'oem7_if'         : LaunchConfiguration('oem7_if'),
                     'oem7_ip_addr'    : LaunchConfiguration('oem7_ip_addr'),
-                    'oem7_port'       : LaunchConfiguration('oem7_port')
+                    'oem7_port'       : LaunchConfiguration('oem7_port'),
+                    
+                    # Topic Parameters
+                    'oem7_position_source' : '', # If not set, then uses INSPVA or BESTPOS based on quality
+                    'oem7_imu_rate'   : 0,
+                    'oem7_odometry_zero_origin' : False,
+                    'oem7_odometry_transform' : False,
+
+                    # Debug/Other Parameters
+                    'oem7_receiver_log_file' : '',
+                    'oem7_decoder_log_file' : '',
+                    'oem7_strict_receiver_init' : True,
+                    'oem7_publish_unknown_oem7raw' : False,
+                    'oem7_publish_delay' : 0.0
+                    
                     },
                     get_override_params() # Must be last to override
                     ],
@@ -62,9 +78,9 @@ def generate_launch_description():
         output='screen',
     )
     
-    ip_arg   = arg('oem7_ip_addr', '192.168.100.201',               'IP Address of Oem7 Receiver, e.g. 192.168.1.2')
-    port_arg = arg('oem7_port',   '3005',              'TCP or UDP port, e.g. 3002')
-    if_arg   = arg('oem7_if',     'Oem7ReceiverTcp',   'Interface Type: Oem7ReceiverTcp or Oem7ReceiverUdp')
+    ip_arg   = arg('oem7_ip_addr', '192.168.100.201',  'IP Address of Oem7 Receiver, e.g. 192.168.1.2')
+    port_arg = arg('oem7_port',    '3005',             'TCP or UDP port, e.g. 3002')
+    if_arg   = arg('oem7_if',      'Oem7ReceiverTcp',  'Interface Type: Oem7ReceiverTcp or Oem7ReceiverUdp')
     
     return LaunchDescription([ip_arg, port_arg, if_arg, 
                               node])
